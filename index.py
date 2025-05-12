be2244344ce27a9e5496b02f18ad7431dff48682
Nonaka <c0b221183f@edu.teu.ac.jp>
2025-05-12 23:50:16 +0900

    add: 入力指示を関数化

--- index.py
+++ index.py
@@ -7,6 +7,15 @@ QUESTION_NUM = 3   # 文章を入力する回数
 input_data = []     # 入力されたデータ
 text = []           # 入力するテキスト(漢字、ひらがな、ローマ字の順)
 
+# 入力指示を表示する関数
+def display_guide(QUESTION_NUM, question_cnt, text_cnt, text_num ):
+  os.system('cls')
+  print(f"[{ question_cnt }/{ QUESTION_NUM }] 表示された文字を入力してください。")
+  print(f"　入力　： { text[text_num]['kanji'] }")
+  print(f"　かな　： { text[text_num]['kana'] }")
+  print(f"ローマ字： { text[text_num]['rome'] }")
+  print("　　　　　 " + ( "-" * text_cnt ))
+
 # メロスデータを読み込み
 with open('merosu_short.csv', mode='r', encoding='utf-8') as f:
   with open('merosu_short_kana.csv', mode='r', encoding='utf-8') as kf:
@@ -27,9 +36,7 @@ with open('merosu_short.csv', mode='r', encoding='utf-8') as f:
 question_cnt = 1                        # 現在の問題番号
 text_cnt = 0                            # 入力文字数
 text_num = random.randint(0, len(text)) # 入力する文章(とりあえずランダム)
-print(f"　入力　： { text[text_num]['kanji'] }")
-print(f"　かな　： { text[text_num]['kana'] }")
-print(f"ローマ字： { text[text_num]['rome'] }")
+display_guide(QUESTION_NUM, question_cnt, text_cnt, text_num )
 while True:
   input_key = keyboard.read_key()
   if input_key != "":
@@ -40,12 +47,7 @@ while True:
     # 正しい入力だった場合、結果を保存
     if input_key == text[text_num]['rome'][text_cnt]:
       text_cnt += 1
-      os.system('cls')
-      print(f"[{ question_cnt }/{ QUESTION_NUM }] 表示された文字を入力してください。")
-      print(f"　入力　： { text[text_num]['kanji'] }")
-      print(f"　かな　： { text[text_num]['kana'] }")
-      print(f"ローマ字： { text[text_num]['rome'] }")
-      print("　　　　　 " + ( "-" * text_cnt ))
+      display_guide(QUESTION_NUM, question_cnt, text_cnt, text_num )
       input_data.append({'key':input_key, 'timestamp':time.time()})
       
       if text_cnt >= len(text[text_num]['rome']):
@@ -57,11 +59,7 @@ while True:
           question_cnt += 1
           text_cnt = 0
           text_num = random.randint(0, len(text))
-          os.system('cls')
-          print(f"[{ question_cnt }/{ QUESTION_NUM }] 表示された文字を入力してください。")
-          print(f"　入力　： { text[text_num]['kanji'] }")
-          print(f"　かな　： { text[text_num]['kana'] }")
-          print(f"ローマ字： { text[text_num]['rome'] }")
+          display_guide(QUESTION_NUM, question_cnt, text_cnt, text_num )
 
 # 結果を表示
 print("----------")
